99de38f133dbab0eddc726460baec07f168f0756
Joshua P Casey <joshua.p.casey@gmail.com>
2021-08-02 12:21:22 -0400

    heroku deploy v.05

--- db.js
+++ db.js
@@ -2,6 +2,11 @@ require('dotenv').config();
 
 const {Sequelize} = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING);
+const sequelize = new Sequelize(process.env.DB_CONNECTION_STRING ||
+    `postgresql://postgres:${encodeURIComponent(process.env.PASS)}@localhost/ryl`,
+    {
+        dialect: 'postgres',
+    }
+);
 
 module.exports = sequelize;
